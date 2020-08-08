3aa239edccf6be26e391a8a12eee64448a3252e6
davidch3 <davidch3@139.com>
2020-08-08 17:44:32 +0800

    Add files via upload

--- drop_pg_temp/drop_pg_temp.sh
+++ drop_pg_temp/drop_pg_temp.sh
@@ -2,7 +2,34 @@
 
 source /usr/local/greenplum-db/greenplum_path.sh
 
-psql postgres -Atc "select datname from pg_database where datname != 'template0'" | while read a; do echo "check for ${a}";psql -Atc "select 'drop schema if exists ' || nspname || ' cascade;' from (select nspname from pg_namespace where nspname like 'pg_temp%' except  select 'pg_temp_' || sess_id::varchar from pg_stat_activity) as foo;" ${a} | psql -a ${a}; done
+LEAKED_SCHEMA_QUERY="
+    SELECT 'DROP SCHEMA IF EXISTS '||schema||';' FROM (
+      SELECT distinct nspname as schema
+        FROM (
+          SELECT nspname, replace(nspname, 'pg_temp_','')::int as sess_id
+          FROM   gp_dist_random('pg_namespace')
+          WHERE  nspname ~ '^pg_temp_[0-9]+'
+          UNION ALL
+          SELECT nspname, replace(nspname, 'pg_toast_temp_','')::int as sess_id
+          FROM   gp_dist_random('pg_namespace')
+          WHERE  nspname ~ '^pg_toast_temp_[0-9]+'
+        ) n LEFT OUTER JOIN pg_stat_activity x using (sess_id)
+        WHERE x.sess_id is null
+        UNION
+        SELECT nspname as schema
+        FROM (
+          SELECT nspname, replace(nspname, 'pg_temp_','')::int as sess_id
+          FROM   pg_namespace
+          WHERE  nspname ~ '^pg_temp_[0-9]+'
+          UNION ALL
+          SELECT nspname, replace(nspname, 'pg_toast_temp_','')::int as sess_id
+          FROM   pg_namespace
+          WHERE  nspname ~ '^pg_toast_temp_[0-9]+'
+        ) n LEFT OUTER JOIN pg_stat_activity x using (sess_id)
+        WHERE x.sess_id is null
+      ) foo;
+    "
 
-psql postgres -Atc "select datname from pg_database where datname != 'template0'" | while read a; do echo "check for ${a}";psql -Atc " select 'drop schema if exists ' || nspname || ' cascade;' from (select nspname from gp_dist_random('pg_namespace') where nspname like 'pg_temp%' except select 'pg_temp_' || sess_id::varchar from pg_stat_activity) as foo;" ${a} | psql -a ${a}; done
+psql postgres -Atc "select datname from pg_database where datname != 'template0'" | while read a; do echo "check for ${a}";psql -Atc "${LEAKED_SCHEMA_QUERY}" ${a} | psql -a ${a}; done
+
 
