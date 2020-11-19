defb79e38f517b0296f39934acaf963c5ac11c35
davidch3 <davidch3@139.com>
2020-11-19 13:27:26 +0800

    Add files via upload

--- lock/v_check_lock_tables_6.sql
+++ lock/v_check_lock_tables_6.sql
@@ -5,20 +5,22 @@ drop view if exists v_check_lock_tables;
 create or replace view v_check_lock_tables
 as
 select tab1.tablename lock_table,tab1.lockmode wait_lockmode,tab1.usename wait_user,tab1.pid wait_procpid,tab1.query_start wait_start,
-       now()-tab1.query_start wait_time,tab1.current_query wait_sql,tab2.lockmode run_lockmode,tab2.usename run_user,tab2.pid run_procpid,
-       tab2.query_start run_start,now()-tab2.query_start run_time,tab2.current_query run_sql,tab2.state run_state
+       tab1.wait_time,tab1.current_query wait_sql,tab2.lockmode run_lockmode,tab2.usename run_user,tab2.pid run_procpid,
+       tab2.query_start run_start,tab2.run_time,tab2.current_query run_sql,tab2.state run_state
 from (
-select locktype,relation::regclass as tablename,aaa.pid,mode as lockmode,usename,bbb.query_start,substr(bbb.query,1,100) as current_query
+select locktype,relation::regclass as tablename,aaa.pid,mode as lockmode,usename,bbb.query_start,substr(bbb.query,1,100) as current_query,
+       now()-bbb.query_start as wait_time
 from pg_locks aaa
 inner join pg_stat_activity bbb on aaa.pid=bbb.pid
 where aaa.granted=false and aaa.relation>30000 and aaa.gp_segment_id=-1 and aaa.locktype='relation'
 and aaa.mode<>'ShareLock' and bbb.waiting=true
 ) tab1
 inner join (
-select locktype,relation::regclass as tablename,aaa.pid,mode as lockmode,usename,bbb.query_start,substr(bbb.query,1,100) as current_query,bbb.state
+select locktype,relation::regclass as tablename,aaa.pid,mode as lockmode,usename,bbb.query_start,substr(bbb.query,1,100) as current_query,bbb.state,
+       now()-bbb.query_start as run_time
 from pg_locks aaa
 inner join pg_stat_activity bbb on aaa.pid=bbb.pid
 where aaa.granted=true and aaa.relation>30000 and aaa.gp_segment_id=-1 and aaa.locktype='relation'
 and aaa.mode<>'ShareLock'
-) tab2 on tab1.tablename=tab2.tablename and now()-tab1.query_start<now()-tab2.query_start;
+) tab2 on tab1.tablename=tab2.tablename and tab1.wait_time<tab2.run_time;
 
