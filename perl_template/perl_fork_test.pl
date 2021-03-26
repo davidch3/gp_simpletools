268a2ccd5b39a23ed3cf0f57f17b7270cec7ad05
davidch3 <davidch3@139.com>
2021-03-26 18:31:20 +0800

    Add files via upload

--- perl_template/perl_fork_test.pl
+++ perl_template/perl_fork_test.pl
@@ -17,10 +17,6 @@ my $concurrency=$ARGV[0];
 my $itotal=$ARGV[1];
 my $num_proc = 0;
 my $num_finish = 0;
-my $pid;
-my $childpid;
-#my $collect;
-my $icalc;
 my $mainpid=$$;
 
 
@@ -49,67 +45,75 @@ sub handler {
 
 
 ####################### MAIN #######################
-for ($icalc=0; $icalc<$itotal; $icalc++){
-  
-  $pid=fork();
-  if(!(defined ($pid))) {
-    print "Can not fork a child process!!!\n$!\n";
-    exit(-1);
-  }
-  $childpid=$$;    
-  
-  if ($pid==0) {
-    #Child process
+sub main{
+  my $pid;
+  my $childpid;
+  my $icalc;
+
+  for ($icalc=0; $icalc<$itotal; $icalc++){
     
-    print "I'm a child process, pid=".$childpid."=\n";
-    if ($childpid%5 == 0) {
-      sleep(13);
-      print "I'm a child process, pid=".$childpid."=. I will exit -1\n";
-      exit -1;
-    } elsif ($childpid%7 == 0) {
-      `psql -d testdb -ac "select count(*) from to_cdr_partname_varchar_spec33;"`;
-      print "I'm a child process, pid=".$childpid."=. I run psql. I will exit 0\n";
-      exit -1;
-    } elsif ($childpid%9 == 0) {
-      `gpcheck ~/allhsots`;
-      print "I'm a child process, pid=".$childpid."=. I run gpcheck. I will exit 0\n";
-      exit -1;
-    } else  {
-      sleep(9);
-      print "I'm a child process, pid=".$childpid."=. I will exit 0\n";
-      exit 0;
+    $pid=fork();
+    if(!(defined ($pid))) {
+      print "Can not fork a child process!!!\n$!\n";
+      exit(-1);
     }
-  
-  } else {                         
-    #Parent process
-    $num_proc++;
-    if ($num_finish%10 == 0) {
-      print "Child process count [".$num_proc."], finish count[".$num_finish."/".$itotal."]\n";
+    $childpid=$$;    
+    
+    if ($pid==0) {
+      #Child process
+      
+      print "I'm a child process, pid=".$childpid."=\n";
+      if ($childpid%5 == 0) {
+        sleep(13);
+        print "I'm a child process, pid=".$childpid."=. I will exit -1\n";
+        exit -1;
+      } elsif ($childpid%7 == 0) {
+        `psql -d testdb -ac "select count(*) from to_cdr_partname_varchar_spec33;"`;
+        print "I'm a child process, pid=".$childpid."=. I run psql. I will exit 0\n";
+        exit -1;
+      } elsif ($childpid%9 == 0) {
+        `gpcheck ~/allhsots`;
+        print "I'm a child process, pid=".$childpid."=. I run gpcheck. I will exit 0\n";
+        exit -1;
+      } else  {
+        sleep(9);
+        print "I'm a child process, pid=".$childpid."=. I will exit 0\n";
+        exit 0;
+      }
+    
+    } else {                         
+      #Parent process
+      $num_proc++;
+      if ($num_finish%10 == 0) {
+        print "Child process count [".$num_proc."], finish count[".$num_finish."/".$itotal."]\n";
+      }
+      do {
+        sleep(1);
+      } until ( $num_proc < $concurrency );
     }
-    ##Collect zombies(collect exit child process)
-    #if ( ($itotal-$num_proc-$num_finish) > 0 ) {
-    #  while ( waitpid(-1, WNOHANG) > 0 ) { $num_finish++; }
-    #}
-    do {
-      sleep(1);
-    } until ( $num_proc < $concurrency );
   }
+    
+  print "waiting for all child finished!\n";
+  my $ichd=0;
+  do {
+    while ( ($ichd=waitpid(-1, WNOHANG)) > 0 ) { $num_finish++; }
+    sleep(1);
+  } until ( $ichd < 0 );
   
+  #my $ret=0;
+  #my $rettmp=`psql -ac "select now();"`;
+  #print $rettmp."\n";
+  #$ret=$?>>8;
+  #print $ret."\n";
   
+  return 0;
 }
 
 
-#waiting for all child finished;
-do {
-  print "Child process count [".$num_proc."], finish count[".$num_finish."/".$itotal."]\n";
-  sleep(1);
-} until($num_proc==0);
+
+my $ret = main();
+exit($ret);
 
 
-my $ret=0;
-my $rettmp=`psql -ac "select now();"`;
-print $rettmp."\n";
-$ret=$?>>8;
-print $ret."\n";
 
 
