37c75f4f4fe65605af98ba0224dc128ef7309c13
davidch3 <davidch3@139.com>
2022-07-19 17:43:54 +0800

    Add files via upload

--- gpssh-exkeys_gp6/gpssh-exkeys_gp6.sh
+++ gpssh-exkeys_gp6/gpssh-exkeys_gp6.sh
@@ -1,45 +1,64 @@
 #!/bin/bash
-set -o errexit
+set +o errexit
 
-if [[ $# -lt 2  ]]; then
+if [ $# -ne 2 ] && [ $# -ne 4 ]; then
   echo "invalid parameters!"
-  echo "usage:`basename $0` -f allhosts "
+  echo "
+Usage:`basename $0` -f allhosts 
+   or 
+      `basename $0` -f allhosts -e allexpands"
   exit 1; 
 fi
 
-while getopts f: opts
+while getopts "f:e": opts
 do
         case $opts in
         f) allhosts_FILENAME=$OPTARG 
            ;;
+        e) allexpands_FILENAME=$OPTARG 
+           ;;
         \?)
            echo "invalid parameters!"  
-           echo "usage:`basename $0` -f allhosts "
+           echo "
+Usage:`basename $0` -f allhosts 
+   or 
+      `basename $0` -f allhosts -e allexpands"
            exit 1;
                 ;;
         esac 
 done
+
+DATETIME=`date +%Y%m%d%H%M%S`
 
 if [ ! -f "$allhosts_FILENAME" ]; then
   echo "Hostfile \"$allhosts_FILENAME\" does not exists!!!"
   exit 1
 fi
 
-read -p "Please input user password of all hosts: " passwd
+USERNAME=`whoami`
 
-if [ -f "$HOME/.ssh/id_rsa" ]; then
-  echo "$HOME/.ssh/id_rsa file exists ... key generation skipped"
-else
+read -p "Please input <$USERNAME> password of all hosts: " passwd
+
+if [ "$allexpands_FILENAME"x = ""x ]; then
   ssh-keygen -t rsa -N "" -f $HOME/.ssh/id_rsa -q
+  if [ -f "$HOME/.ssh/known_hosts" ]; then
+    echo "Clean up known_hosts"
+    cp $HOME/.ssh/known_hosts $HOME/.ssh/known_hosts.bak$DATETIME
+    > $HOME/.ssh/known_hosts
+  fi
 fi
 
-HOSTLIST=`cat $allhosts_FILENAME |grep -v "^$" |grep -v "^#"`
+if [ "$allexpands_FILENAME"x != ""x ]; then
+  HOSTLIST=`cat $allexpands_FILENAME |grep -v "^$" |grep -v "^#"`
+else
+  HOSTLIST=`cat $allhosts_FILENAME |grep -v "^$" |grep -v "^#"`
+fi
 for hostname in $HOSTLIST
 do
   echo "[$hostname] Add known_host and ssh-copy-id ..."
-  ssh-keygen -R $hostname 1>/dev/null 2>/dev/null
-  ssh-keyscan -H $hostname 2>/dev/null >> $HOME/.ssh/known_hosts 
-  sshpass -p $passwd ssh-copy-id $hostname 1>/dev/null
+  ssh-keygen -R $hostname 
+  ssh-keyscan -H $hostname 1>> $HOME/.ssh/known_hosts 
+  sshpass -p $passwd ssh-copy-id -i $HOME/.ssh/id_rsa.pub $hostname
 done
 
 echo "Running gpssh-exkeys ..."
