6f00d6ca0fc11d6cb0eb6cc5ecbf9a8bdb88dce2
davidch3 <davidch3@139.com>
2023-06-12 13:29:28 +0800

    Add files via upload

--- killsession/gp_kill_idle.sh
+++ killsession/gp_kill_idle.sh
@@ -20,14 +20,14 @@ loging (){
 # Logging all idle in transaction for more than X hours
   psql -t -ac "select usename,procpid,current_query,query_start,backend_start,xact_start from pg_stat_activity where current_query='<IDLE> in transaction' and now()-xact_start>interval '${INT_IDLE_TRAN} hours'" postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
 # Logging all the connections which were idle for more than X hours
-  psql -t -ac "select usename,procpid,current_query,query_start,backend_start,xact_start from pg_stat_activity where current_query='<IDLE>' and now()-backend_start>interval '${INT_IDLE_CONN} hours'" postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
+  psql -t -ac "select usename,procpid,current_query,query_start,backend_start,xact_start from pg_stat_activity where current_query='<IDLE>' and now()-query_start>interval '${INT_IDLE_CONN} hours'" postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
 }
 
 idle_conn () {
 # Terminate pid's of IDLE CONNECTIONS for more than X hours :  
   psql -A -t -c "SELECT 'select pg_terminate_backend('||procpid||');select pg_sleep(1);' from pg_stat_activity where current_query = '<IDLE> in transaction' and now()-xact_start>interval '${INT_IDLE_TRAN} hours'" postgres | psql -a postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
 # Terminate pid's of IDLE CONNECTIONS for more than X hours :
-  psql -A -t -c "SELECT 'select pg_terminate_backend('||procpid||');select pg_sleep(1);' from pg_stat_activity where current_query = '<IDLE>' and now()-backend_start>interval '${INT_IDLE_CONN} hours'" postgres | psql -a postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
+  psql -A -t -c "SELECT 'select pg_terminate_backend('||procpid||');select pg_sleep(1);' from pg_stat_activity where current_query = '<IDLE>' and now()-query_start>interval '${INT_IDLE_CONN} hours'" postgres | psql -a postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
 }
 
 date >> /home/gpadmin/gpAdminLogs/gp_kill_idle_${LOGDATE}.log
