829b647ba38d7f40954008cc3cc46ac6de8c39d5
davidch3 <davidch3@139.com>
2024-12-11 11:25:49 +0800

    Add files via upload

--- killsession/gp_kill_idle_gp6.sh
+++ killsession/gp_kill_idle_gp6.sh
@@ -11,23 +11,23 @@
 #            The connection to the server was lost. Attempting reset: Succeeded.
 #########################################################################################
 
-INT_IDLE_TRAN=1
-INT_IDLE_CONN=24
+INT_IDLE_TRAN=3
+INT_IDLE_CONN=12
 LOGDATE=`date +%Y%m%d`
 source /usr/local/greenplum-db/greenplum_path.sh
 
 loging (){
 # Logging all idle in transaction for more than X hours
-  psql -t -ac "select usename,pid,query,query_start,backend_start,xact_start from pg_stat_activity where state='idle in transaction' and now()-xact_start>interval '${INT_IDLE_TRAN} hours'" postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
+  psql -t -ac "select usename,pid,query,query_start,backend_start,xact_start from pg_stat_activity where state='idle in transaction' and now()-state_change>interval '${INT_IDLE_TRAN} hours'" postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
 # Logging all the connections which were idle for more than X hours
-  psql -t -ac "select usename,pid,query,query_start,backend_start,xact_start from pg_stat_activity where state='idle' and now()-query_start>interval '${INT_IDLE_CONN} hours'" postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
+  psql -t -ac "select usename,pid,query,query_start,backend_start,xact_start from pg_stat_activity where state='idle' and now()-state_change>interval '${INT_IDLE_CONN} hours'" postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
 }
 
 idle_conn () {
 # Terminate pid's of IDLE CONNECTIONS for more than X hours :  
-  psql -A -t -c "SELECT 'select pg_terminate_backend('||pid||');select pg_sleep(1);' from pg_stat_activity where state = 'idle in transaction' and now()-xact_start>interval '${INT_IDLE_TRAN} hours'" postgres | psql -a postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
+  psql -A -t -c "SELECT 'select pg_terminate_backend('||pid||');select pg_sleep(1);' from pg_stat_activity where state = 'idle in transaction' and now()-state_change>interval '${INT_IDLE_TRAN} hours'" postgres | psql -a postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
 # Terminate pid's of IDLE CONNECTIONS for more than X hours :
-  psql -A -t -c "SELECT 'select pg_terminate_backend('||pid||');select pg_sleep(1);' from pg_stat_activity where state = 'idle' and now()-query_start>interval '${INT_IDLE_CONN} hours'" postgres | psql -a postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
+  psql -A -t -c "SELECT 'select pg_terminate_backend('||pid||');select pg_sleep(1);' from pg_stat_activity where state = 'idle' and now()-state_change>interval '${INT_IDLE_CONN} hours'" postgres | psql -a postgres >> /home/gpadmin/gpAdminLogs/kill_long_idle_${LOGDATE}.log
 }
 
 date >> /home/gpadmin/gpAdminLogs/gp_kill_idle_${LOGDATE}.log
